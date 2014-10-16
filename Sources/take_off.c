aa23b4d256a449c9bac176505d39752173422423
TILAK Pierre-Louis <pierre.tilak@gmail.com>
2014-10-16 16:04:56 +0200

    Trame du programme SPRINT 1 espérant faire décoller le drone

---
+++ Sources/take_off.c
@@ -0,0 +1,8 @@
+#include"take_off.h"
+
+DEFINE_THREAD_ROUTINE(take_off, data)
+{
+        // Code de la function
+    
+        //Return (THREAD_RET) 0;
+}
---
+++ Sources/take_off.h
@@ -0,0 +1,2 @@
+#include <VP_Api/vp_api_thread_helper.h> // api pour les threads
+PROTO_THREAD_ROUTINE(take_off, data);
