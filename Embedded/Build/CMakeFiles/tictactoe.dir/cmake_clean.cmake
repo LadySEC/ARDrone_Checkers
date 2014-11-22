4faf3b070ac96f03a679277d70d8fe1aed1e2f72
Pierre-Louis TILAK <tilak@crunchbang>
2014-11-22 15:26:18 +0100

    ajout périodic.c

---
+++ Embedded/Build/CMakeFiles/tictactoe.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tictactoe.dir/home/tilak/ARDRONE/ARDrone_TicTacToe/Embedded/Sources/at_command.c.o"
+  "CMakeFiles/tictactoe.dir/home/tilak/ARDRONE/ARDrone_TicTacToe/Embedded/Sources/communication.c.o"
+  "CMakeFiles/tictactoe.dir/home/tilak/ARDRONE/ARDrone_TicTacToe/Embedded/Sources/keyboard.c.o"
+  "CMakeFiles/tictactoe.dir/home/tilak/ARDRONE/ARDrone_TicTacToe/Embedded/Sources/main.c.o"
+  "CMakeFiles/tictactoe.dir/home/tilak/ARDRONE/ARDrone_TicTacToe/Embedded/Sources/supervisor.c.o"
+  "CMakeFiles/tictactoe.dir/home/tilak/ARDRONE/ARDrone_TicTacToe/Embedded/Sources/periodic.c.o"
+  "CMakeFiles/tictactoe.dir/home/tilak/ARDRONE/ARDrone_TicTacToe/Embedded/Sources/kbd_control.c.o"
+  "tictactoe.pdb"
+  "tictactoe"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/tictactoe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
