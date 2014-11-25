c19877a0cd2e5f6346e889b38f8b0b7746bfe8fd
Pierre-Louis TILAK <tilak@crunchbang>
2014-11-25 18:38:44 +0100

    Adaptation detect_tag

--- Embedded/Sources/detect_tag.cpp
+++ Embedded/Sources/detect_tag.cpp
@@ -1,51 +1,35 @@
-#include "detect_tag.hpp"
-
+#include "detect_tag.h"
 
 // @function detectAndDisplay
-    
-    
-Position detect(std::string i_color, std::string i_form)
+Position detect( Mat i_frame)
 {
 
-    Mat i_frame; 
+    // Get the cosines of all corners
+    vector<double> cos;
 
     Position new_pos ;
     new_pos.found = 0 ;
 
-    int angles ;
+    int angles = 0;
+    int i = 0;
 
-    Mat HSV  ;
-    cvtColor(i_frame,HSV,CV_BGR2HSV);
 
-    Mat im_color ;
-    if (i_color == "blue")
-        inRange(HSV,cv::Scalar(100,50,100),cv::Scalar(150,255,255),im_color);
-    else if (i_color == "red")
-        inRange(HSV,cv::Scalar(0,150,100),cv::Scalar(20,255,255),im_color);
-    else if (i_color == "green")
-        inRange(HSV,cv::Scalar(30,50,50),cv::Scalar(80,255,255),im_color);
 
     // Use Canny instead of threshold to catch squares with gradient shading
     Mat bw ;
-    Canny(im_color, bw, 0,50,5) ;
+    //Canny(i_frame, bw, 0,50,5) ;
 
-    //Find contours
-    vector<vector<Point> > contours;
-    //findContours(bw.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+	Canny(i_frame, bw, 0,50,5);
     
+	//Find contours
+    vector<vector<Point> > contours;
+
     Mat bw_clone = bw.clone() ;
     findContours(bw_clone, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
     vector<Point> approx;
 
-    if (i_form == "triangle")
-        angles = 3 ;
-    else if (i_form == "rectangle")
-        angles = 4 ;
-    else if (i_form == "circle")
-        angles = 0 ;
 
-
-    for (int i = 0; i < contours.size(); i++)
+  for (int i = 0; i < contours.size(); i++)
     {
         // Approximate contour with accuracy proportional
         // to the contour perimeter
@@ -55,14 +39,14 @@ Position detect(std::string i_color, std::string i_form)
         if (fabs(contourArea(contours[i])) < 100 || !isContourConvex(approx))
             continue;
 
-        if ( (approx.size() == 3) && (i_form == "triangle") )
+        if ( (approx.size() == 3) )
         {
             cv::Rect r = cv::boundingRect(contours[i]);
             new_pos.abs = r.x + (r.width/2);
             new_pos.ord = r.y + (r.height/2);
             new_pos.found = 1 ;
         }
-        else if ( (approx.size() ==4) && (i_form == "rectangle"))
+        else if ( (approx.size() ==4))
         {
             // Get the cosines of all corners
             vector<double> cos;
@@ -83,7 +67,7 @@ Position detect(std::string i_color, std::string i_form)
                 new_pos.found = 1 ;
             }
         }
-        else if ( i_form == "circle")
+        else
         {
             // Detect and label circles
             double area = contourArea(contours[i]);
@@ -102,8 +86,6 @@ Position detect(std::string i_color, std::string i_form)
     return new_pos ;
 }
 
-
-
 // Helper function to find a cosine of angle between vectors from pt0->pt1 and pt0->pt2
 static double angle(Point pt1, Point pt2, Point pt0)
 {
@@ -113,4 +95,3 @@ static double angle(Point pt1, Point pt2, Point pt0)
     double dy2 = pt2.y - pt0.y;
     return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
 }
-
