09791be787b2186652c556ede0f594ad2cf10bf5
FabriceR <rakotonarivofabrice@gmail.com>
2014-11-25 20:00:48 +0100

    derniere partie reorganisation pilou hugo

---
+++ Embedded/Build/CMakeFiles/tictactoe.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/jose/Embedded/Sources/at_command.c" "/home/jose/Embedded/Build/CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/at_command.c.o"
+  "/home/jose/Embedded/Sources/calcul_order.c" "/home/jose/Embedded/Build/CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/calcul_order.c.o"
+  "/home/jose/Embedded/Sources/calcul_tag.c" "/home/jose/Embedded/Build/CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/calcul_tag.c.o"
+  "/home/jose/Embedded/Sources/communication.c" "/home/jose/Embedded/Build/CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/communication.c.o"
+  "/home/jose/Embedded/Sources/keyboard.c" "/home/jose/Embedded/Build/CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/keyboard.c.o"
+  "/home/jose/Embedded/Sources/main.c" "/home/jose/Embedded/Build/CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/main.c.o"
+  "/home/jose/Embedded/Sources/periodic.c" "/home/jose/Embedded/Build/CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/periodic.c.o"
+  "/home/jose/Embedded/Sources/supervisor.c" "/home/jose/Embedded/Build/CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/supervisor.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jose/Embedded/Sources/detect_tag.cpp" "/home/jose/Embedded/Build/CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/detect_tag.cpp.o"
+  "/home/jose/Embedded/Sources/detect_tag_wrapper.cpp" "/home/jose/Embedded/Build/CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/detect_tag_wrapper.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "../Sources"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ Embedded/Build/CMakeFiles/tictactoe.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/at_command.c.o"
+  "CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/calcul_order.c.o"
+  "CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/calcul_tag.c.o"
+  "CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/communication.c.o"
+  "CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/detect_tag_wrapper.cpp.o"
+  "CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/detect_tag.cpp.o"
+  "CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/keyboard.c.o"
+  "CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/periodic.c.o"
+  "CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/supervisor.c.o"
+  "CMakeFiles/tictactoe.dir/home/jose/Embedded/Sources/main.c.o"
+  "tictactoe.pdb"
+  "tictactoe"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/tictactoe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
