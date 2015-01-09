881657d4b4aad9981494d415334b4dc078135b91
FabriceR <rakotonarivofabrice@gmail.com>
2015-01-09 16:42:43 +0100

    images IHM windows for prayers

--- Supervisor/Sources/Tcp_client/global.h
+++ Supervisor/Sources/Tcp_client/global.h
@@ -25,24 +25,15 @@
     static const QString STOP_SCENARIO_PATH = "../tags/stop.jpg" ;
 
 #elif defined(_WIN32) || defined(WIN32)   /* _Win32 is usually defined by compilers targeting 32 or   64 bit Windows systems */
-    static const QString ICON_RED_RECTANGLE_PATH = "..\\tags\\red_rectangle.jpg";
-    static const QString ICON_BLUE_RECTANGLE_PATH = "..\\tags\\blue_rectangle.jpg";
-    static const QString ICON_GREEN_RECTANGLE_PATH = "..\\tags\\green_rectangle.jpg";
-    static const QString ICON_RED_TRIANGLE_PATH = "..\\tags\\red_triangle.jpg";
-    static const QString ICON_BLUE_TRIANGLE_PATH = "..\\tags\\blue_triangle.jpg";
-    static const QString ICON_GREEN_TRIANGLE_PATH = "..\\tags\\green_triangle.jpg";
-    static const QString ICON_RED_CIRCLE_PATH = "..\\tags\\red_circle.jpg";
-    static const QString ICON_BLUE_CIRCLE_PATH = "..\\tags\\blue_circle.jpg";
-    static const QString ICON_GREEN_CIRCLE_PATH = "..\\tags\\green_circle.jpg";
-    static const QString DRONE_IMAGE_PATH = "..\\tags\\drone.png";
-    static const QString PATH_TO_CIRCLE = "..\\tags\\black_circle.png" ;
-    static const QString PATH_TO_CROSS = "..\\tags\\black_cross.png" ;
-    static const QString EMERGENCY_BUTTON_PATH = "..\\tags\\emergency.jpg" ;
-    static const QString START_PATH = "..\\tags\\drone_takeoff.png";
-    static const QString STOP_PATH = "..\\tags\\drone_landing.png" ;
-    static const QString RESET_PATH = "..\\tags\\reset.jpg" ;
-    static const QString START_SCENARIO_PATH = "..\\tags\\start.jpg" ;
-    static const QString STOP_SCENARIO_PATH = "..\\tags\\stop.jpg" ;
+    static const QString DRONE_IMAGE_PATH = "..\\..\\tags\\drone.png";
+    static const QString PATH_TO_CIRCLE = "..\\..\\tags\\black_circle.png" ;
+    static const QString PATH_TO_CROSS = "..\\..\\tags\\black_cross.png" ;
+    static const QString EMERGENCY_BUTTON_PATH = "..\\..\\tags\\emergency.jpg" ;
+    static const QString START_PATH = "..\\..\\tags\\drone_takeoff.png";
+    static const QString STOP_PATH = "..\\..\\tags\\drone_landing.png" ;
+    static const QString RESET_PATH = "..\\..\\tags\\reset.jpg" ;
+    static const QString START_SCENARIO_PATH = "..\\..\\tags\\start.jpg" ;
+    static const QString STOP_SCENARIO_PATH = "..\\..\\tags\\stop.jpg" ;
 
 #endif
 
